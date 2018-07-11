f82fcf607efa11fc8b9b2999bdcd161ece7d5a25
harsh <harsh.wadhwa.182@gmail.com>
2018-07-11 18:12:12 +0530

    changes to make image full width or width fit but not completed

--- app/components/Posts/index.js
+++ app/components/Posts/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { PostsContainer, postsContainer } from "../../containers";
 import { redditHelper } from "../../helpers";
-import {
-  GridRow,
-  TouchableOpacity,
-  Card,
-  Image,
-  View,
-  Subtitle,
-  ImageBackground,
-  ListView,
-  Tile,
-  Divider
-} from "@shoutem/ui";
+import { TouchableOpacity, ListView } from "@shoutem/ui";
 import { Subscribe } from "unstated";
+import { Dimensions, Image } from "react-native";
+
+const window = Dimensions.get("window");
 
 class Posts extends React.Component {
   async componentWillMount() {
@@ -25,11 +17,19 @@ class Posts extends React.Component {
     } catch (error) {}
   }
   renderRow(post) {
+    console.log("post: ", post);
+    post.preview.images[0].resolutions.forEach(resolution1 => {
+      if (resolution1.width < window.width) {
+        resolution = resolution1;
+      }
+    });
+    console.log("resolution: ", resolution);
     return (
       <TouchableOpacity key={post.id}>
-        <ImageBackground
-          styleName="large"
-          source={{ uri: post.preview.images[0].source.url }}
+        <Image
+          style={{ flex: 1, width: window.width }}
+          // styleName="large-portrait"
+          source={{ uri: resolution && resolution.url }}
         />
       </TouchableOpacity>
     );
